1e9d80cf94d6e31e76124b943173147c36600a3a
billy784512 <billy784512369@gmail.com>
2025-05-30 23:40:28 +0800

    feat: basic ws listening

--- ws/index.js
+++ ws/index.js
@@ -5,23 +5,43 @@ import { Server } from 'socket.io';
 const app = express();
 const server = createServer(app);
 const io = new Server(server, {
-  cors: { origin: '*' }
+    cors: { origin: '*' }
 });
 
 io.on('connection', (socket) => {
-  console.log(`Client connected: ${socket.id}`);
+    console.log(`Client connected: ${socket.id}`);
 
-  socket.on('message', (data) => {
-    console.log('Received:', data);
-    socket.emit('message', `Echo: ${data}`);
-  });
+    socket.on('enter-room', (data) => {
+        socket.join(data.room_id)
+    })
 
-  socket.on('disconnect', () => {
-    console.log(`Client disconnected: ${socket.id}`);
-  });
+    socket.on('pick-color', (data) => {
+        io.to(data.room_id).emit('receive-color', {
+            sender: socket.id,
+            message: {
+                user_id: data.user_id,
+                color: data.color
+            }
+        })
+    })
+    
+    socket.on('stroke', (data) => {
+        io.to(data.room_id).emit('receive-stroke', {
+            sender: socket.id,
+            message: {
+                user_id: data.user_id,
+                color: data.color,
+                path: data.path  // array of {x, y}
+            }
+        });
+    });
+
+    socket.on('disconnect', () => {
+        console.log(`Client disconnected: ${socket.id}`);
+    });
 });
 
 const PORT = 3000;
 server.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`);
+    console.log(`Server listening at http://localhost:${PORT}`);
 });
